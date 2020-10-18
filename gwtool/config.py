41fe3e3f9124e3b37e36949ed65352c14be1a9fb
Stephen Zhang <stephenpcg@gmail.com>
2020-10-18 14:44:24 +0800

    implement config module

---
+++ gwtool/config.py
@@ -0,0 +1,155 @@
+import yaml
+import ipaddress
+from pathlib import Path
+
+from gwtool.env import env, logger
+
+
+class InterfaceConfig:
+    """
+    Interfaces can be discovered by pyroute2, however, we can provide more info on how to setup gateway.
+
+    Currently the following properties can be configured:
+    * devgroup: when invoking `ifaceup`, the interface devgroup will be set. Gwtool has 4 forcely defined groups,
+                wan(1), lan(2), guest(3), tunnel(4). These 4 groups can be configured by name, others by number.
+    * gateway: Gateway address of this interface. Some interfaces does not need gateway address, like ppp, tun, etc.
+    """
+    def __init__(self, name, devgroup=None, gateway=None, table=None, **kwargs):
+        self.name = name
+
+        # TODO Support user defined group names in /etc/iproute2/group
+        devgroup = {'wan': 1, 'lan': 2, 'guest': 3, 'tunnel': 4}.get(devgroup, None)
+        if not (devgroup is None or isinstance(devgroup, int)):
+            logger.error(f'[InterfaceConfig] Invalid devgroup valud: {devgroup}')
+            raise ValueError(f'Invalid devgroup value: {devgroup}')
+        self.devgroup = devgroup
+
+        # Gateway address must be valid ip address (not cidr).
+        # ip_address() supports both v4 and v6, however we currently only consider v6.
+        if gateway:
+            try:
+                ipaddress.ip_address(gateway)
+            except ipaddress.AddressValueError:
+                logger.error(f'[InterfaceConfig] Invalid gateway value (must be valid ip address): {gateway}')
+                raise
+        self.gateway = gateway
+
+    def validate(self, **kwargs):
+        return True
+
+
+class GatewayConfig:
+    """
+    A gateway is a next hop used in route entry. Gwtool creates gateway intances automatically from interfaces,
+    addition to that, user can define custom gateways or change how interface gateway work.
+
+    The following properties can be used:
+    * link: indicates this gateway is alias of another gateway. It can be useful for making route table reads clear.
+    * interface: if this gateway consists of only one interface, configure it via this property. If interface name
+      is same as gateway name, it can be omitted.
+    * interfaces: if this gateway consists of more than one interfaces, config via this property. This property
+      conflicts with interface, you should only use one of them.
+    * gateway: for single interface gateway, we can override gateway address defined on the interface.
+    """
+    def __init__(self, name, link=None, interface=None, interfaces=None, gateway=None, **kwargs):
+        self.name = name
+
+        # TODO should check if the link name exists
+        self.link = link
+
+        if interface and interfaces:
+            logger.error('[GatewayConfigure] interface and interfaces are exclusive')
+            raise ValueError('"interface" and "interfaces" can not be used together')
+        if gateway and interfaces:
+            logger.error('[GatewayConfigure] interface and interfaces are exclusive')
+            raise ValueError('"gateway" and "interfaces" can not be used together')
+
+        # TODO should check if interface exists
+        if interfaces:
+            self.single_interface_mode = False
+            self.interfaces = interfaces
+        else:
+            self.single_interface_mode = True
+            self.interface = interface or name
+            self.gateway = gateway
+
+    def validate(self, *, gateways, interfaces, **kwargs):
+        # TODO
+        return True
+
+
+class RouteTableConfig:
+    """
+    Custom route tables.
+    """
+    def __init__(self, table, entries, **kwargs):
+        self.table = table
+        self.entries = entries
+
+    def validate(self, *, netzones, **kwargs):
+        # TODO
+        return True
+
+
+class RouteRuleConfig:
+    """
+    Custom route rules.
+    """
+    def __init__(self, rule, **kwargs):
+        self.rule = rule
+
+    def validate(self, **kwargs):
+        return True
+
+
+class Config:
+    """
+    Represents the whole gateway.yaml config file
+    """
+    def __init__(self, config_file):
+        if config_file.exists():
+            try:
+                with config_file.open(encoding='utf8') as fp:
+                    content = yaml.safe_load(fp)
+            except Exception:
+                logger.error(f'Error loading gateway config file: {config_file}')
+                raise
+        else:
+            logger.info(f'Config file does not exist: {config_file}, assume empty config')
+            content = {}
+
+        self.interfaces = {}
+        for name, config in content.get('interfaces', {}).items():
+            self.interfaces[name] = InterfaceConfig(name, **config)
+
+        self.gateways = {}
+        for name, config in content.get('gateways', {}).items():
+            self.gateways[name] = GatewayConfig(name, **config)
+
+        self.route_tables = {}
+        for table, entries in content.get('routing', {}).get('tables', {}).items():
+            self.route_tables[table] = RouteTableConfig(table, entries)
+
+        self.route_rules = []
+        for rule in content.get('routing', {}).get('rules', []):
+            self.route_rules.append(RouteRuleConfig(rule=rule))
+
+        netzone_search_path = []
+        paths = content.get('netzone_search_path', [])
+        if not isinstance(paths, list):
+            logger.error('Config Error: netzone_search_path must be list of paths')
+            raise ValueError('Invalid netzone_search_path, must be list of paths')
+        for path in paths:
+            netzone_search_path.append(Path(path))
+        netzone_search_path.append(env.codespace / 'netzones')
+        self.netzone_search_path = netzone_search_path
+
+    def validate(self, **kwargs):
+        for interface in self.interfaces.values():
+            interface.validate(**kwargs)
+        for gateway in self.gateways.values():
+            gateway.validate(**kwargs)
+        for table in self.route_tables.values():
+            table.validate(**kwargs)
+        for rule in self.route_rules.valus():
+            rule.validate(**kwargs)
--- gwtool/env.py
+++ gwtool/env.py
@@ -6,10 +6,6 @@ from pathlib import Path
 from gwtool.utils import cached_property
 
 
-class NotConfigured:
-    pass
-
-
 class Env:
     """
     Env holds some global variables for paths.
@@ -17,26 +13,32 @@ class Env:
     def __init__(self):
         self._configured = False
 
-        self._workspace = NotConfigured
-        self._config_file = NotConfigured
-        self._log_file = NotConfigured
+        # Codespace is where this code lived, we have some builtin data stored here (e.g. netzone files).
+        self.codespace = Path(__file__).parent
 
-        self.logger = logging.getLogger(self.cmdline_name)
+        if sys.argv[0] in ['python', 'python3', 'ipython', 'ipython3']:
+            logger_name = 'shell'
+        else:
+            logger_name = Path(sys.argv[0]).stem
+
+        self._log_format = '[%(asctime)s][%(name)s][%(levelname)s]: %(message)s'
+        self.logger = logging.getLogger(logger_name)
         self.logger.setLevel(logging.INFO)
 
-        if self.is_interactive:
+        # if running interactively, add stream handler to logger
+        if sys.stdout.isatty():
             self._add_log_stream()
 
     def configure(self, *, workspace=None, config_file=None, log_file=None):
         if self._configured:
             raise Exception('Env already configured.')
 
-        if workspace:
-            self._workspace = Path(workspace)
-        if config_file:
-            self._config_file = Path(config_file)
-        if log_file:
-            self._log_file = Path(log_file)
+        # Workspace stores instance configuration and scripts. It usually is '/opt/gateway'.
+        self.workspace = Path(workspace or '/opt/gateway')
+        # Config file is the core configuration to gwtool. It usually is {workspace}/gateway.yaml.
+        self.config_file = config_file and Path(config_file) or self.workspace / 'gateway.yaml'
+        # Gwtool is usually run by triggers (e.g. if-up), we try to log important messages for trouble shooting.
+        self.log_file = log_file and Path(log_file) or self.workspace / 'var/log/gwtool.log'
 
         # we have configured, so let's enable file logging
         self._add_log_file(self.log_file)
@@ -44,63 +46,17 @@ class Env:
         self._configured = True
 
     @cached_property
-    def codespace(self):
-        """
-        Codespace is where this code lived, we have some builtin data stored here (e.g. netzone files).
-        """
-        return Path(__file__).parent
+    def gwconfig(self):
+        if not self._configured:
+            raise Exception('Env not configured, can not access gwconfig.')
 
-    @property
-    def workspace(self):
-        """
-        Workspace stores instance configuration and scripts. It usually is '/opt/gateway'.
-        """
-        if self._workspace is NotConfigured:
-            return Path('/opt/gateway')
-        return self._workspace
-
-    @property
-    def config_file(self):
-        """
-        Config file is the core configuration to gwtool. It usually is {workspace}/gateway.yaml.
-        """
-        if self._config_file is NotConfigured:
-            return self.workspace / 'gateway.yaml'
-        return self._config_file
-
-    @property
-    def log_file(self):
-        """
-        Gwtool is usually run by triggers (e.g. if-up), we try to log important messages for trouble shooting.
-        """
-        if self._log_file is NotConfigured:
-            return self.workspace / 'var/log/gwtool.log'
-        return self._log_file
-
-    @cached_property
-    def is_interactive(self):
-        """
-        Where currently is running interactively (script is invoked manually) or in backend (invoked by triggers).
-        """
-        return sys.stdout.isatty()
-
-    @cached_property
-    def cmdline_name(self):
-        """
-        The command line name, used by logger name.
-        """
-        if sys.argv[0] in ['python', 'python3', 'ipython', 'ipython3']:
-            return 'shell'
-        return Path(sys.argv[0]).stem
-
-    @cached_property
-    def log_format(self):
-        return '[%(asctime)s][%(name)s][%(levelname)s]: %(message)s'
+        from gwtool.config import Config
+        return Config(self.config_file)
 
     def _add_log_stream(self):
         handler = logging.StreamHandler()
         handler.setLevel(logging.DEBUG)
-        handler.setFormatter(logging.Formatter(self.log_format))
+        handler.setFormatter(logging.Formatter(self._log_format))
 
         self.logger.addHandler(handler)
 
@@ -111,7 +67,7 @@ class Env:
 
         handler = logging.handlers.TimedRotatingFileHandler(filename=logfile, when='W0', encoding='utf8')
         handler.setLevel(logging.DEBUG)
-        handler.setFormatter(logging.Formatter(self.log_format))
+        handler.setFormatter(logging.Formatter(self._log_format))
 
         self.logger.addHandler(handler)
 
